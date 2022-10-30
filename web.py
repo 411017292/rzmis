5fbef20090ba4d49fa9b5c5ea4d6b99b077eb7b8
411017292 <113697123+411017292@users.noreply.github.com>
2022-10-30 17:54:33 +0800

    Update web.py

--- web.py
+++ web.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>楊子青Python網頁</h1>"
+    homepage = "<h1>黃睿哲Python網頁</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
     homepage += "<a href=/welcome?nick=tcyang>傳送使用者暱稱</a><br>"
-    homepage += "<a href=/about>子青簡介網頁</a><br>"
+    homepage += "<a href=/about>睿哲簡介網頁</a><br>"
     return homepage
 
 @app.route("/mis")
